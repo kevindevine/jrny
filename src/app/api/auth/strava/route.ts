e0a151424129c36b1e4cec4be59f9a33a08bb2dc
kevindevine <kevdevine@gmail.com>
2025-07-05 20:37:46 +0100

    Strava auth reroute

--- src/app/api/auth/strava/route.ts
+++ src/app/api/auth/strava/route.ts
@@ -1,4 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
+import { getStravaAuthUrl } from '@/lib/strava';
+
 export async function GET() {
-  return NextResponse.redirect("https://www.strava.com/oauth/authorize?client_id=166639&redirect_uri=https://jrny-isxg31ac6-kevin-devines-projects.vercel.app/api/auth/strava/callback&response_type=code&scope=read,activity:read_all");
+  const authUrl = getStravaAuthUrl();
+  return NextResponse.redirect(authUrl);
 }
